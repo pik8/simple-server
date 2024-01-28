c123a94304885a5b6a81725f1eb1e305584ad57b
Ana Margarida Picoito <anamargaridapic8@gmail.com>
2024-01-28 00:28:12 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    console.log('request was made: ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    const myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+    myReadStream.pipe(res);
+});
+
+server.listen(3000, '127.0.0.1');
+console.log('Pics, Im listening on port 3000');
